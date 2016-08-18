c1ab131e07a48cfc37e929ebc6514f7d32c81437
Stefan Corneliu Petrea <stefan@garage-coding.com>
2016-08-18 13:00:47 +0300

    update

---
+++ src/main/java/com/mdetect/Analyzer.java
@@ -0,0 +1,24 @@
+package com.mdetect;
+
+public class Analyzer {
+	/*
+	 * This class will go through existing codebases and precompute
+	 * a set of common characteristics.
+	 * 
+	 * For example, the most common class/function/method/variable
+	 * names.
+	 * 
+	 * It will also compute file sizes and checksums.
+	 * 
+	 */
+	
+	public  Analyzer() {
+		
+	}
+	
+	void run() {
+		
+	}
+	
+	
+}
--- src/main/java/com/mdetect/App.java
+++ src/main/java/com/mdetect/App.java
@@ -1,7 +1,19 @@
 package com.mdetect;
 
 public class App {
-	   public static void main(String[] args) {
-	        System.out.println("Hello World!"); // Display the string.
-	   }
+	/*
+	 * So far, we want to cover these:
+	 * - excessive use of chr()
+	 * - excessive use of ord()
+	 * - long base64 encoded strings
+	 * - use of the base64_decode PHP function
+	 * 
+	 */
+
+	 public static void main(String[] args) {
+	  
+	      System.out.println("Hello World!"); // Display the string.
+	 }
 }
+
+
