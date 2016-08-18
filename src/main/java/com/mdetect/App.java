e41dc10f6898a0639c1544b7c079625d8a4dec20
Stefan Corneliu Petrea <stefan@garage-coding.com>
2016-08-18 15:01:19 +0300

    update

--- src/main/java/com/mdetect/App.java
+++ src/main/java/com/mdetect/App.java
@@ -1,14 +1,7 @@
 package com.mdetect;
 
 public class App {
-	/*
-	 * So far, we want to cover these:
-	 * - excessive use of chr()
-	 * - excessive use of ord()
-	 * - long base64 encoded strings
-	 * - use of the base64_decode PHP function
-	 * 
-	 */
+
 
 	 public static void main(String[] args) {
 	  
--- src/main/java/com/mdetect/Detector.java
+++ src/main/java/com/mdetect/Detector.java
@@ -1,5 +1,21 @@
 package com.mdetect;
 
 public class Detector {
-
+	/*
+	 * So far, we want to cover these:
+	 * - does not contain common names from 
+	 *   Joomla/Wordpress/Drupal ; instead contains names 
+	 *   that appear to be random (high) 
+	 * - excessive use of chr() (medium)
+	 * - excessive use of ord() (medium)
+	 * - makes use of hex-escaped characters (low)
+	 * - excessive use of string concatenation (high)
+	 * - long base64 encoded strings (high)
+	 * - use of the base64_decode PHP function (high)
+	 * 
+	 */
+	
+	public Detector() {
+		
+	}
 }
