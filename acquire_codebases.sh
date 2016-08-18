fda4c218e8e3a4833ba9ac1bf64ce9055cfdb7d5
Stefan Corneliu Petrea <stefan@garage-coding.com>
2016-08-18 09:46:05 +0300

    update

--- acquire_codebases.sh
+++ acquire_codebases.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+#
+#
 # Acquires PHP codebases for major PHP projects used
 # by a vast majority of websites
 #
+#
 
-git clone https://github.com/joomla/joomla-cms.git
-git clone https://github.com/drupal/drupal.git
-git clone https://github.com/WordPress/WordPress.git
+git clone --depth 100 https://github.com/joomla/joomla-cms.git
+git clone --depth 100 https://github.com/drupal/drupal.git
+git clone --depth 100 https://github.com/WordPress/WordPress.git
 
 
