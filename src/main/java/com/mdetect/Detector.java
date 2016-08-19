53248b564f69a6334f41f76419dcc430476147d6
Stefan Corneliu Petrea <stefan@garage-coding.com>
2016-08-19 09:19:25 +0300

    update

--- src/main/java/com/mdetect/Detector.java
+++ src/main/java/com/mdetect/Detector.java
@@ -2,6 +2,8 @@ package com.mdetect;
 
 public class Detector {
 	/*
+	 * Note: The grammar works for PHP up to 5.6
+	 * 
 	 * So far, we want to cover these:
 	 * - does not contain common names from 
 	 *   Joomla/Wordpress/Drupal ; instead contains names 
@@ -12,7 +14,7 @@ public class Detector {
 	 * - excessive use of string concatenation (high)
 	 * - long base64 encoded strings (high)
 	 * - use of the base64_decode PHP function (high)
-	 * 
+	 * - presence of eval() (high)
 	 */
 	
 	public Detector() {
