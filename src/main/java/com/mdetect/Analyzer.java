0a93515919feea458caa73765c0638bdb96f7063
Stefan Corneliu Petrea <stefan@garage-coding.com>
2016-08-20 17:23:12 +0300

    update

--- src/main/java/com/mdetect/Analyzer.java
+++ src/main/java/com/mdetect/Analyzer.java
@@ -7,19 +7,33 @@ import java.sql.Statement;
 
 public class Analyzer {
 	/*
-	 * This class will go through existing codebases and precompute
-	 * a set of common characteristics.
+	 * This class contains minimal logic that does a set of
+	 * very specific sqlite operations.
+	 * 
+	 * In particular, it connects to a sqlite database, and then it
+	 * precomputes a series of characteristics of known codebases
+	 * and stores them in the sqlite database.
 	 * 
 	 * For example, the most common class/function/method/variable
-	 * names.
-	 * 
-	 * It will also compute file sizes and checksums.
+	 * names, file sizes and checksums.
 	 * 
 	 */
-	
+	public String dbPath = null;
+	public Connection connection = null;
 	public  Analyzer() {
-		
-	}
+		dbPath = "/tmp/sample.db";
+        try
+        {
+          connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+          Statement statement = connection.createStatement();
+          statement.setQueryTimeout(30);
+        }
+        catch(SQLException e)
+        {
+          System.err.println(e.getMessage());
+          System.exit(-1);
+        };
+    }
 	
 	void run() {
 		
