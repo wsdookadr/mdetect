05e7c6f33d70d65a1d6214d7e8d73ae67096c1e4
Stefan Corneliu Petrea <stefan@garage-coding.com>
2016-08-24 03:52:16 +0300

    added a counter class

---
+++ src/main/java/com/mdetect/CountMap.java
@@ -0,0 +1,21 @@
+package com.mdetect;
+
+import java.util.HashMap;
+
+public class CountMap extends HashMap<String,Integer> {
+	
+	/*
+	 * increase the count for this key
+	 */
+	public void add(String o) {
+		int count;
+		if(this.containsKey(o)) {
+			count = ((Integer) this.get(o)).intValue() + 1; 
+		} else {
+			count = 1;
+		}
+		
+		super.put(o, count);
+	}
+
+}
